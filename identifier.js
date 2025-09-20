2988565fbfde2cf55e269cbf38fa1e8ee8435308
goudsneha <snehagoud1994@gmail.com>
2025-09-20 14:51:39 -0500

    feature/identifers basic example

---
+++ identifier.js
@@ -0,0 +1,15 @@
+var x =100;
+x = 50;//can be re-assigned
+console.log("var x =",x);
+
+//let example
+
+let y = 12;
+y = 33 //can be re-assigned
+console.log("lety =",y);
+
+//const example
+
+const c = 6;
+//cannot be reassigned
+console.log("const= " ,c);
